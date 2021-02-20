61fbe9add6e574e311a03eddb58e6910a9bc995d
hibeeeekay <60797412+hibeeeekay@users.noreply.github.com>
2021-02-20 13:21:37 +0100

    Update

--- phpProject/myCode.php
+++ phpProject/myCode.php
@@ -4,6 +4,6 @@
      $pin = (mt_rand(100,999)).(mt_rand(1000,9999)).(mt_rand(10000000,99999999))."<br>";
      echo "ETISALAT"."<br>";
      echo "Pin No: " .$pin."<br>";  
+     $number++;
  }
- $number++;
 ?>
